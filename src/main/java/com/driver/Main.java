6871895687b7367f41960e64aebcd492728e2e31
Ahinsak Raj <ahinsakraj@gmail.com>
2023-08-10 16:54:13 +0530

    assignment completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+
+//        obj.name = "ahinsak";
+//        System.out.println(obj.name);
+//
+//        error
+//        java: name has private access in com.driver.RWOnly
+//        as name is private data member so we can not accesss it using dot operator
+        obj.setName("ahinsak");
+        String name = obj.getName();
+//        System.out.println(obj.getName());
+
+    }
   
 }
